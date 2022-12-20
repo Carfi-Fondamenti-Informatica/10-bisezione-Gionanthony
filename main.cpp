fcad4ae29f48bc7ff6bec2670bd36d76219192a0
Gionanthony <117936434+Gionanthony@users.noreply.github.com>
2022-12-20 18:37:21 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@ float f (float y) {
 }
 
 int main() {
-    float a, b, x;
+    float a, b, err, x;
 
     do {
         cout << "inserire estremi" << endl;
@@ -16,30 +16,24 @@ int main() {
         cin >> b;
     } while (f(a)*f(b)>=0);
 
-    x = ((a+b)/2);
+    do {
+        x = ((a+b)/2);
 
-    if (f(x) == 0) {
-        cout << x << endl;
-        cout << f(x) << endl;
-    } else {
-        if (f(a) * f(b) < 0) {
-            b = x;
-
-        } else {
-            a = x;
-        }
-    }
-
-    if (abs((b - a) / 2) <= 0.000001) {
-        cout << x << endl;
-        cout << f(x) << endl;
-    } else {
-        while (abs((b - a) / 2) >= 0.000001) {
-            x = (a + b) / 2;
+        if (f(x) == 0) {
             cout << x << endl;
             cout << f(x) << endl;
-            break;
+        } else {
+            if (f(a) * f(b) < 0) {
+                b = x;
+
+            } else {
+                a = x;
             }
-        }
+            err = abs((b - a) / 2);
+    }} while (abs(err>=1e-6));
+
+    cout << x << endl;
+    cout << f(x) << endl;
+    
     return 0;
     }
