c1730dde918845ffd65651d57be78c08facc6442
Gionanthony <117936434+Gionanthony@users.noreply.github.com>
2022-12-22 12:55:50 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -16,24 +16,30 @@ int main() {
         cin >> b;
     } while (f(a)*f(b)>=0);
 
-    do {
-        x = ((a+b)/2);
 
-        if (f(x) == 0) {
+        do {
+            x = ((a + b) / 2);
+
+            if (f(x) == 0) {
+                cout << x << endl;
+                cout << f(x) << endl;
+                break;
+            } else {
+                if (f(a) * f(b) < 0) {
+                    b = x;
+                    err = abs((b - a) / 2);
+
+                } else {
+                    a = x;
+                    err = abs((b - a) / 2);
+                }
+            }
+            
             cout << x << endl;
             cout << f(x) << endl;
-        } else {
-            if (f(a) * f(b) < 0) {
-                b = x;
-
-            } else {
-                a = x;
-            }
-            err = abs((b - a) / 2);
-    }} while (abs(err<=1e-6));
-
-    cout << (a+b)/2 << endl;
-    cout << f((a+b)/2) << endl;
+            
+        } while (err <= 1e-6);
+            
 
     return 0;
-    }
+}
