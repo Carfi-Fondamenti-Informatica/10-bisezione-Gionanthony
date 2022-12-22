bdaaad7c42e8a646d47ba7b73db4af01962ef477
Gionanthony <117936434+Gionanthony@users.noreply.github.com>
2022-12-22 16:06:24 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -32,43 +32,8 @@ int main() {
                 err = abs ((b-a)/2);
             }
         } while (err >= 1e-6);
-    
-    #include <iostream>
-#include "cmath"
-using namespace std;
-
-float f (float y) {
-    float R;
-    R = pow(y, 2) * (cos(y)) + 1;
-    return R;
-}
-
-int main() {
-    float a, b, err, x;
-
-    do {
-        cout << "inserire estremi" << endl;
-        cin >> a >> b;
-    } while ((f(a)*f(b))>=0);
-
-        do {
-            x = ((a + b) / 2);
-
-            if (f(x) == 0) {
-                cout << x << endl;
-                cout << f(x) << endl;
-                return 0;
-            } else {
-                if (f(a) * f(b) < 0) {
-                    b = x;
-                } else {
-                    a = x;
-                }
-                err = abs ((b-a)/2);
-            }
-        } while (err >= 1e-6);
         
- cout << int (x * 10000) / 10000.0 << endl;
+    cout << int (x * 10000) / 10000.0 << endl;
     
     return 0;
 }
